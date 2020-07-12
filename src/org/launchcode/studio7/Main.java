d1ade40e0fa28da3eaef5226e8e49f9bf3e2851f
EricSoto <ericsoto926@gmail.com>
2020-07-12 14:24:00 -0400

    studio 7

--- src/org/launchcode/studio7/Main.java
+++ src/org/launchcode/studio7/Main.java
@@ -1,13 +1,13 @@
 package org.launchcode.studio7;
 
-import org.launchcode.studio7.CD;
+
 
 public class Main {
 
 
     public static void main(String[] args){
 
-        CD mixTape =  new CD("launchCodeAlbum", 5.8, "RW", true);
+        //CD mixTape =  new CD("launchCodeAlbum", 5.8, "RW", true);
 
         DVD movie =  new DVD("launchCodeMovie", 10.2, "RW", false);
 
